44a964ecdd4472eec84dbeafe4ad1fd106d83b28
Hari Raghav <94678323+Raghav-1548@users.noreply.github.com>
2022-05-16 06:20:09 +0000

    Done

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -3,3 +3,8 @@ hrs = float(input("Enter Hours:"))
 rph=float(input("Enter Rate: "))
 gpay=hrs*rph
 print("Pay:", str(gpay))
+
+def input():
+  hrs = float(input("Enter Hours:"))
+  rph=float(input("Enter Rate: "))
+  
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,14 +1,21 @@
+h=float(input("Enter Hours: "))
+r=float(input("Enter rate: "))
+
+
 def computepay(h, r):
 	p=h*r
 	if h>40:
 		p=40*r+1.5*(r)*(h-40)
 	return p
 
-h=float(input("Enter Hours: "))
-r=float(input("Enter rate: "))
-if h<=40:
+def output(h,r):
+  if h<=40:
     pay=h*r
     print(pay)
-else:
-	p = computepay(h, r)
-	print("Pay", p)
+  else:
+	  p = computepay(h, r)
+	  print("Pay", p)
+
+computepay(h, r)
+output(h, r)
+
